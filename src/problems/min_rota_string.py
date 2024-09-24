19ae7501783f36b313439e5d15dd820251ffa974
Ramachandran Kulothungan <ramachandran.kulo@gmail.com>
2024-09-24 02:17:05 -0400

    minimum in rotated string

---
+++ src/problems/min_rota_string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums) -> int:
+        n = len(nums)
+        
+        left = 0
+        right = n-1
+        minimum = nums[left]
+        while left<right:
+            print(left, right)
+            if nums[left]<=nums[right]:
+                minimum = min(minimum, nums[left])
+                break
+            m = int((left + right) / 2)
+            if nums[m] > nums[left]:
+                left = m
+            else:
+                minimum = min(minimum, nums[m])
+                right = m
+        return minimum
+
+print(Solution().findMin([x for x in range(5,10)]+[x for x in range(1,5)]))
