57fcfb29c1ca1452b8d8df4d1e2dfa6d88a805e2
Ramachandran Kulothungan <ramachandran.kulo@gmail.com>
2024-09-26 02:24:38 -0400

    binary search rotated array

---
+++ src/problems/bin_search_rota_arr.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums, target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n-1
+        while left<right:
+            print(left, right)
+            m = int((left + right) / 2)
+            if nums[m]==target:
+                return m
+            if nums[right] > nums[m]:
+                if target > nums[m] and target <= nums[right]:
+                    left = m+1
+                else:
+                    right = m
+            else:
+                if target < nums[m] and target >= nums[left]:
+                    right = m
+                else:
+                    left = m+1
+        return -1
+
+print(Solution().search([4,5,6,7,0,1,2], 0))
+
+
+
