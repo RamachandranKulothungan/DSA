e4c605f78bdf96348b841d0451f8be1c3e8df150
Ramachandran Kulothungan <ramachandran.kulo@gmail.com>
2024-10-22 16:00:39 -0400

    lca bst

---
+++ src/problems/binary_tree/lowest_common_ancestor_bst.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        temp = root
+        while temp:
+            if temp.val == p.val or temp.val == q.val:
+                # print(temp.val)
+                return temp
+            if temp.val > p.val and temp.val > q.val:
+                temp = temp.left
+                continue
+            if temp.val < p.val and temp.val < q.val:
+                temp = temp.right
+                continue
+            return temp
